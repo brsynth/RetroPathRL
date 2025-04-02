a68addbd2afefce31d2ee456f817496cecfd08a5
Thomas Duigou <thomas.duigou@inrae.fr>
2025-04-02 16:00:01 +0200

    style(config): refine

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ It replaces what I previously wanted to define as 'global'
 try:
     from rp3_dcache.Manager import Manager  # In house module
     from rp3_dcache.Utils import make_document_id, as_document, rdmols_from_document
-
     dcache_installed = True
 except ModuleNotFoundError:
     dcache_installed = False
@@ -29,30 +28,22 @@ DB_time = 0
 if DB_CACHE:
     global CACHE_MGR
     if add_Hs:
-        CACHE_MGR = Manager(replace=DB_REPLACE, collection="results_with_H")
+        CACHE_MGR = Manager(replace=DB_REPLACE, collection = "results_with_H")
     else:
-        CACHE_MGR = Manager(replace=DB_REPLACE, collection="results_without_H")
+        CACHE_MGR = Manager(replace=DB_REPLACE, collection = "results_without_H")
     CACHE_MGR.connect()
-    DB_config = "Setting the DB from config file: Installed package: {}. Using cache DB: {}; Replacing results: {}".format(
-        dcache_installed, DB_CACHE, DB_REPLACE
-    )
+    DB_config = "Setting the DB from config file: Installed package: {}. Using cache DB: {}; Replacing results: {}".format(dcache_installed, DB_CACHE, DB_REPLACE)
 elif dcache_installed:
-    DB_config = "Setting the DB from config file: Installed package: {}. Using cache DB: {}; Replacing results: {}".format(
-        dcache_installed, DB_CACHE, DB_REPLACE
-    )
+    DB_config = "Setting the DB from config file: Installed package: {}. Using cache DB: {}; Replacing results: {}".format(dcache_installed, DB_CACHE, DB_REPLACE)
 else:
-    DB_config = "Setting the DB from config file: Installed package: {}".format(
-        dcache_installed
-    )
+    DB_config = "Setting the DB from config file: Installed package: {}".format(dcache_installed)
 
 # Mode for using RP3: retrosynthesis or biosensor. QSAR might be implemented one day.
 global retrosynthesis
 global biosensor
 retrosynthesis = True
 biosensor = False
-tree_mode_config = "Using retrosynthesis: {} - using biosensor {}".format(
-    retrosynthesis, biosensor
-)
+tree_mode_config = "Using retrosynthesis: {} - using biosensor {}".format(retrosynthesis, biosensor)
 
 # Configuring local cache. Could be replaced by a proper caching system one day.
 global home_made_cache
@@ -61,9 +52,7 @@ home_made_cache = {}
 global use_cache
 use_cache = False
 
-cache_config = "Initialising an empty cache: {}; Using it: {}".format(
-    home_made_cache, use_cache
-)
+cache_config = "Initialising an empty cache: {}; Using it: {}".format(home_made_cache, use_cache)
 
 # MCTS parameters for configuration
 
@@ -75,16 +64,13 @@ transposition_table = {}
 use_transpositions = False
 use_transpositions_depth = False
 
-transposition_table_config = "Using transposition tables: {}. With depth: {}".format(
-    use_transpositions, use_transpositions_depth
-)
+transposition_table_config = "Using transposition tables: {}. With depth: {}".format(use_transpositions, use_transpositions_depth)
 
 # For toxicity, using log(IC50) as penaly when below 0.
 global use_toxicity
 try:
     import sklearn
     from sklearn.neural_network import MLPRegressor
-
     sklearn_here = True
 except ModuleNotFoundError:
     toxicity_config = "Toxicity will not be enabled because sklearn is not installed"
